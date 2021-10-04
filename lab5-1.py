669b6fea7e5d258087700f5e4567751fd909dce4
p22yxi <p22yxi@fairfieldprep.org>
2021-10-04 09:57:01 -0400

    lab5

---
+++ lab5-1.py
@@ -0,0 +1,16 @@
+# Author : Yongdong Xi (Oct 4)
+
+# int(a)
+# name error
+
+# int('a')
+# value error
+
+# 'a'+2
+# type error
+
+# import date
+# module not found error
+
+# SyntaxError
+# print("I'm a happy camper!)
